4d09f34178e44ddacc694b40c371724ab4239b66
chaiyd <chaiyd.cn@gmail.com>
2021-07-12 14:37:53 +0800

    bugfix

--- wkcrm/docker/wkcrm.sh
+++ wkcrm/docker/wkcrm.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 server_names=('gateway' 'work' 'oa' 'authorization' 'admin' 'bi' 'crm'  'job' 'examine')
 
 cd /opt
-# shellcheck disable=SC2039
 for value in "${server_names[@]}"
 do
-    # shellcheck disable=SC2164
     cd /opt/$value
     sh 72crm.sh start
     sleep 10s;
